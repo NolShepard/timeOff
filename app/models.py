b9b13b56cdfdc789d374d4b95f5a0f3d9c6cdbb0
Myrtle <nol@pc.lan>
2020-12-01 16:36:28 -0600

    increased sign on security, register page, and updated db

--- app/models.py
+++ app/models.py
@@ -1,20 +1,33 @@
-from datetime import datetime
-from app import db
+from datetime import datetime, date
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    requests = db.relationship('Dateoff', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
         
-class Request(db.Model):
+class Dateoff(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.DateTime, index=True, unique=True)
+    date = db.Column(db.Date, index=True, unique=True)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
-        return '<Request {}>'.format(self.date)
+        return '<Dateoff {}>'.format(self.date)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
